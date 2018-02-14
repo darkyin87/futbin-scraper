c5aacbdabb616fe11091628a32cb7f2645a0327e
darkyin87 <darkyin87@gmail.com>
2018-02-14 10:07:22 -0800

    Adding sentry integration

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import json
 from fetch_player import fetch_prices
 from mail import EmailAPI
 from sms import GoogleVoiceApi
+from raven.base import Client as SentryClient
 
 
 def process_prices():
-    prices = fetch_prices()
-    prices_str = json.dumps(prices, indent=4, sort_keys=True)
-    print prices_str
-    mail = EmailAPI()
-    mail.send_email(text=prices_str, subject='Player Prices')
-    sms = GoogleVoiceApi()
-    sms.send_text(prices_str)
+    try:
+        prices = fetch_prices()
+        prices_str = json.dumps(prices, indent=4, sort_keys=True)
+        print prices_str
+        mail = EmailAPI()
+        mail.send_email(text=prices_str, subject='Player Prices')
+        sms = GoogleVoiceApi()
+        sms.send_text(prices_str)
+    except Exception:
+        sentry_client = SentryClient()
+        sentry_client.captureException()
